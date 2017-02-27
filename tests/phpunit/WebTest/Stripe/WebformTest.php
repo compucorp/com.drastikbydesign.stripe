8411587545070b60939b04aa7b93af15db68cd03
coldrunKacper <kacper@coldrun.pl>
2017-02-28 00:12:06 +0100

    CGP-695 - Updated WebTest with test files and default test setup.

--- tests/phpunit/WebTest/Stripe/WebformTest.php
+++ tests/phpunit/WebTest/Stripe/WebformTest.php
@@ -1,30 +1,15 @@
 <?php
-/*
- +--------------------------------------------------------------------+
- | CiviCRM version 4.7                                                |
- +--------------------------------------------------------------------+
- | Copyright CiviCRM LLC (c) 2004-2017                                |
- +--------------------------------------------------------------------+
- | This file is a part of CiviCRM.                                    |
- |                                                                    |
- | CiviCRM is free software; you can copy, modify, and distribute it  |
- | under the terms of the GNU Affero General Public License           |
- | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
- |                                                                    |
- | CiviCRM is distributed in the hope that it will be useful, but     |
- | WITHOUT ANY WARRANTY; without even the implied warranty of         |
- | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
- | See the GNU Affero General Public License for more details.        |
- |                                                                    |
- | You should have received a copy of the GNU Affero General Public   |
- | License along with this program; if not, contact CiviCRM LLC       |
- | at info[AT]civicrm[DOT]org. If you have questions about the        |
- | GNU Affero General Public License or the licensing of CiviCRM,     |
- | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
- +--------------------------------------------------------------------+
- */
 
 require_once 'tests/phpunit/CiviTest/CiviSeleniumTestCase.php';
+
+// Bootstrap Drupal.
+$currentPath = getcwd();
+$drupalRoot = explode('/sites/all/', $currentPath)[0];
+set_include_path($drupalRoot . PATH_SEPARATOR . get_include_path());
+define('DRUPAL_ROOT', $drupalRoot);
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
 /**
  * Class WebTest_Event_AddEventTest
@@ -36,14 +21,18 @@ class WebTest_Stripe_WebformTest extends CiviSeleniumTestCase {
   }
 
   public function testWebformPayment() {
+    $this->installPaymentWebform();
+    $webformNode = $this->getWebformNode();
+    $this->assertNotNull($webformNode->nid);
+
     $paymentData = $this->getPaymentTestData();
 
     $this->open($this->sboxPath . '/page/payment-test');
     $this->type('edit-submitted-civicrm-1-contact-1-fieldset-fieldset-civicrm-1-contact-1-email-email', $paymentData['email']);
-    $this->click("op");
-    $this->waitForElementPresent("credit_card_number");
+    $this->click('op');
+    $this->waitForElementPresent('credit_card_number');
     $this->type('credit_card_number', $paymentData['credit_card_number']);
-    $this->type("cvv2", $paymentData['cvv2']);
+    $this->type('cvv2', $paymentData['cvv2']);
     $this->select('credit_card_exp_date[M]', 'value=' . $paymentData['credit_card_exp_date_month']);
     $this->select('credit_card_exp_date[Y]', 'value=' . $paymentData['credit_card_exp_date_year']);
     $this->type('billing_first_name', $paymentData['billing_first_name']);
@@ -54,12 +43,13 @@ class WebTest_Stripe_WebformTest extends CiviSeleniumTestCase {
     $this->select('billing_state_province_id-5', 'value=' . $paymentData['billing_state_province_id']);
     sleep(5);
     $this->type('billing_postal_code-5', $paymentData['billing_postal_code']);
-    $this->click("xpath=//form[@id='webform-client-form-9783']/div/div[@class='form-actions']/input[@value='Submit']");
+    $this->click("xpath=//form[@id='webform-client-form-{$webformNode->nid}']/div/div[@class='form-actions']/input[@value='Submit']");
 
     $this->waitForElementPresent("xpath=//div[@class='webform-confirmation']");
     $this->assertElementContainsText("xpath=//div[@class='webform-confirmation']", "Thank you, your submission has been received.");
 
     $this->screenshot('tests/printscreen/webform-payment-complete.png');
+    node_delete($webformNode->nid);
   }
 
   public function testCiviCRMContributionPayment() {
@@ -80,15 +70,6 @@ class WebTest_Stripe_WebformTest extends CiviSeleniumTestCase {
       'email' => $random . '@jameson.name',
     ]);
     $contactId = $contact['values'][0]['contact_id'];
-
-    /*$this->click("xpath=//li[@id='tab_contribute']/a");
-    //sleep(25);
-    //$this->waitForElementPresent("xpath=//form[@class='CRM_Contribute_Form_Search']");
-    $this->waitForElementPresent("xpath=//form[@id='Search']");
-    $this->assertElementContainsText("xpath=//form[@id='Search']/div[@class='view-content']/div[@class='action-link']/a[2]/span", "Submit Credit Card Transaction");
-    $this->click("xpath=//form[@id='Search']/div[@class='view-content']/div[@class='action-link']/a[2]");
-
-    sleep(20);*/
     
     $this->open($this->sboxPath . "/civicrm/contact/view/contribution?reset=1&action=add&cid={$contactId}&context=contribution&mode=live");
 
@@ -96,7 +77,6 @@ class WebTest_Stripe_WebformTest extends CiviSeleniumTestCase {
     $this->select('payment_processor_id', 'value=' . $stripeProcessorId);
     $this->select('financial_type_id', 'value=' . $financialTypeId);
     $this->type('total_amount', '1');
-    $this->screenshot('tests/printscreen/civicrm-payment4.png');
     $this->select('credit_card_type', 'value=Visa');
     $this->type('credit_card_number', $paymentData['credit_card_number']);
     $this->type('cvv2', $paymentData['cvv2']);
@@ -108,6 +88,12 @@ class WebTest_Stripe_WebformTest extends CiviSeleniumTestCase {
     sleep(5);
     $this->select('billing_state_province_id-5', 'value=' . $paymentData['billing_state_province_id']);
     $this->type('billing_postal_code-5', $paymentData['billing_postal_code']);
+    $this->click('_qf_Contribution_upload');
+
+    $this->waitForElementPresent("xpath=//div[@id='crm-notification-container']/div/div[@class='notify-content']");
+    $this->assertElementContainsText("xpath=//div[@id='crm-notification-container']/div/div[@class='notify-content']", "The transaction record has been processed.");
+
+    $this->screenshot('tests/printscreen/civicrm-payment-complete.png');
   }
 
   private function getPaymentTestData() {
@@ -155,6 +141,17 @@ class WebTest_Stripe_WebformTest extends CiviSeleniumTestCase {
       return NULL;
     }
     return $financialTypes['values'][0]['id'];
+  }
+
+  private function installPaymentWebform() {
+    exec('drush node-export-import --uid=1 < tests/webform/payment_webform.node');
+  }
+
+  private function getWebformNode() {
+    $alias = 'page/payment-test';
+    $path = drupal_lookup_path('source', $alias); 
+    $node = menu_get_object('node', 1, $path);
+    return $node;
   }
 
   private function screenshot($filename) {
