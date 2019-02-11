efb4aa6545cce3c46aaab3f979771c55c663bc09
varshith89 <vinu@compucorp.co.uk>
2019-02-11 14:37:51 +0000

    BASW-412: Code Improvements

--- js/civicrm_stripe.js
+++ js/civicrm_stripe.js
@@ -182,23 +182,25 @@
       }
 
       // card_number not empty validation
-      if (!$form.find("input#credit_card_number").val()) {
-        $form.find("input#credit_card_number").parent().find('.crm-error').remove();
-        $form.find("input#credit_card_number").parent().append('<span class="crm-error msg">Credit-Card Number is required.</span>');
+      var $card_number = $form.find("input#credit_card_number");
+      if (!$card_number.val()) {
+        $card_number.parent().find('.crm-error').remove();
+        $card_number.parent().append('<span class="crm-error msg">Credit-Card Number is required.</span>');
         return false;
       }
       else {
-        $form.find("input#credit_card_number").parent().find('.crm-error.msg').remove();
+        $card_number.parent().find('.crm-error.msg').remove();
       }
 
       // cvv not empty validation
-      if (!$form.find("input#cvv2").val()) {
-        $form.find("input#cvv2").parent().find('.crm-error').remove();
-        $form.find("input#cvv2").parent().append('<span class="crm-error msg">CVV Number is required.</span>');
+      var $cvv = $form.find("input#cvv2");
+      if (!$cvv.val()) {
+        $cvv.parent().find('.crm-error').remove();
+        $cvv.parent().append('<span class="crm-error msg">CVV Number is required.</span>');
         return false;
       }
       else {
-        $form.find("input#cvv2").parent().find('.crm-error.msg').remove();
+        $cvv.parent().find('.crm-error.msg').remove();
       }
 
       // Disable the submit button to prevent repeated clicks, cache button text, restore if Stripe returns error
